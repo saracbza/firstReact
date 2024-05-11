df56268394e70de71d705a70d17584ed1e51cb1d
saracbza <sasa.cb2002@gmail.com>
2024-05-11 16:13:42 -0300

    Alterações para consumir API

--- aplicacao/src/assets/components/linha-task/index.tsx
+++ aplicacao/src/assets/components/linha-task/index.tsx
@@ -1,26 +1,27 @@
-import { ButtonGroup, Flex, Box, Spacer} from "@chakra-ui/react"
-import ButtonTask from "../button-task"
+import { ButtonGroup, Flex, Box, Spacer, Button} from "@chakra-ui/react"
+import { Tarefa } from "../../../interfaces/tasks"
 
 interface Props{
-    titleTask: string
+    task: Tarefa
+    deleteTask(id: number): void
+    changeStatus(task: Tarefa): void
 }
 
-function LinhaTask({titleTask}: Props){
+function LinhaTask({ task, deleteTask, changeStatus }: Props){
     return(
-        <li>
     <Flex maxW={'32rem'} >
         <Box p="13">
-          {titleTask}
+          {task.id} - {task.title}
         </Box>
         <Spacer />
         <Box maxW={'32rem'}>
         <ButtonGroup gap='1'>
-            <ButtonTask size='md' color='red' type='submit' label='Pendente'/>
-            <ButtonTask size='md' color='red' type='submit' label='Excluir'/>
+            <Button onClick={()=> changeStatus(task)} size='md' colorScheme={task.completed ? 'green' : 'orange'}> 
+            {task.completed ? 'Concluída' : 'Pendente'} </Button>
+            <Button size='md' colorScheme='red' onClick={()=> deleteTask(task.id)}> Excluir</Button>
         </ButtonGroup> 
         </Box>
     </Flex>
-    </li>   
     )
 }
 
---
+++ aplicacao/src/interfaces/tasks.ts
@@ -0,0 +1,10 @@
+export interface Tarefa{
+    id: number
+    title: string
+    completed: boolean
+}
+
+export interface PostTask {
+    title: string
+    completed?: boolean
+}
