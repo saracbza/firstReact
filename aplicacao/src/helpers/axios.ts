d42c59aae3e9611aa1f0feecc002318d3798ddab
saracbza <sasa.cb2002@gmail.com>
2024-05-24 19:24:09 -0300

    alterando url da api

--- aplicacao/src/helpers/axios.ts
+++ aplicacao/src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://projetoapi-r37e.onrender.com' //configurando o axios com o link da api
+    baseURL: 'http://localhost:3001' //configurando o axios com o link da api
 })
 
 export default api
